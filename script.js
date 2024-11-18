fdbf943ca03404b044c029a6cc3049712ec346c3
jaswanthchowdary99 <154533631+jaswanthchowdary99@users.noreply.github.com>
2024-11-18 14:34:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function formInfo(event){
+	event.preventDefault();
+	const firstName = document.getElementById("firstName").value;
+	const lastName = document.getElementById("lastName").value;
+	const phone = document.getElementById("phone").value;
+	const email = document.getElementById("email").value;
+	alert(`First Name: ${firstName}`+` Last Name: ${lastName}`+` Phone Number: ${phone}`+` Email ID: ${email}`);
+}
